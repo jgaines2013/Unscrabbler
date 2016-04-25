472bcecdeb10592c9064ea77ad9a6150650a80e1
jonlikesapples <jonlikesapples@gmail.com>
2016-04-25 12:57:30 -0700

    Added files via upload

---
+++ CS146-SCRABBLEPROJECT/src/Trie.java
@@ -0,0 +1,77 @@
+
+public class Trie {
+	private TrieNode root;
+	
+	public Trie()
+	{ root = new TrieNode(); }
+	
+	public void insert(String word)
+	{
+		TrieNode p = root;
+		for (int i = 0; i < word.length(); i++)
+		{
+			char c = word.charAt(i);
+			//c is the nth number of the alphabet
+			//goes into respective index of array
+			int index = c - 'a'; 
+			if (p.arr[index] == null)
+			{
+				TrieNode temp = new TrieNode();
+				//branches off again.
+				p.arr[index] = temp;
+				p = temp;
+			}
+			else
+			{ p=p.arr[index]; }
+		}
+		p.isEnd=true;
+	}
+	
+	public TrieNode searchNode(String word)
+	{
+		TrieNode p = root;
+		for (int i = 0; i < word.length(); i++)
+		{
+			char c = word.charAt(i);
+			int index = c - 'a';
+			if (p.arr[index] != null)
+			{
+				p = p.arr[index];
+			} 
+			else 
+			{
+				return null;
+			}
+		}
+		
+		if (p==root)
+		{return null;}
+		
+		return p;
+	}
+	
+	public boolean startsWith(String prefix)
+	{
+		TrieNode p = searchNode(prefix);
+		if (p==null)
+		{return false;}
+		return true;
+	}
+	
+	public boolean search(String word)
+	{
+		TrieNode p = searchNode(word);
+		if (p==null)
+		{return false;}
+
+		if (p.isEnd)
+		{return true;}
+		
+		return false;
+	}
+	
+}
+
+
+
+
---
+++ CS146-SCRABBLEPROJECT/src/TrieNode.java
@@ -0,0 +1,12 @@
+
+public class TrieNode {
+	//master TrieNode
+	TrieNode[] arr;
+	//signifies 
+	boolean isEnd;
+	
+	public TrieNode()
+	{
+		this.arr = new TrieNode[26];
+	}
+}
