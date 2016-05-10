825e95dd39be380d9857cd91cdd6612985226905
Jonathan Wong <jonlikesapples@gmaill.com>
2016-05-09 22:02:27 -0700

    returns all permutations of handTiles+anchor
    call getAllWords(char anchor) to get an arrayList<String>

---
+++ CS146-SCRABBLEPROJECT/src/handTiles.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class handTiles {
+
+	//to hold hand tiles
+	char[] hand;
+	char[] temp;
---
+++ CS146-SCRABBLEPROJECT/src/tester.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+
+public class tester {
+	public static void main(String[] args)
+	{
+		//NOTE: SCRABBLE MAX HAND SIZE IS 7
+		String hand = "abcdef";
+		char[] testArray = hand.toCharArray();
+		handTiles handtile = new handTiles(hand);
+		System.out.println(Arrays.toString(testArray));
+		handtile.getAllWords('t');
+	}
+}
