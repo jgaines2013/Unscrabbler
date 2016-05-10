0ecc9cedc26f42e9b14047d0b01acddddad79f07
Neil <neil.p.warren@gmail.com>
2016-05-10 16:42:20 -0700

    Updated Position

--- CS146-SCRABBLEPROJECT/src/Position.java
+++ CS146-SCRABBLEPROJECT/src/Position.java
@@ -3,20 +3,21 @@ public class Position
 {
     int i;
     int j;
-    
+    char character;
     /**
      * Creates a data type Position, which holds the column and row values in a 2d array
      * @param i The column index
      * @param j The row index
      */
-    public Position(int i, int j)
+    public Position(int i, int j, char c)
     {
         this.i = i;
         this.j = j;
+        character = c;
     }
     
     public String toString()
     {
-        return String.format("Position: %d, %d", i, j);
+        return String.format("Position: %d, %d  Character: %s", i, j, character);
     }
 }
